419267663b6ee6bcb059747e852f3bc136448886
Jan-Paul Kleemans <jpkleemans@gmail.com>
2022-02-05 22:58:36 +0100

    Fix js

--- src/script.js
+++ src/script.js
@@ -6,8 +6,8 @@ setInterval(function () { updateCurrentLivestream() }, 30000)
 
 function updateCurrentLivestream () {
   var $livestreamIframe = document.getElementById('livestream_iframe')
-  var $gebarentolkLink = document.getElementById('gebarentolk_link')
-  var $gebarentolkButton = document.getElementById('gebarentolk_button')
+  // var $gebarentolkLink = document.getElementById('gebarentolk_link')
+  // var $gebarentolkButton = document.getElementById('gebarentolk_button')
 
   var now = new Date()
 
@@ -31,8 +31,8 @@ function updateCurrentLivestream () {
       }
 
       $livestreamIframe.src = urls.regulier
-      $gebarentolkLink.href = urls.gebarentolk
-      $gebarentolkButton.href = urls.gebarentolk
+      // $gebarentolkLink.href = urls.gebarentolk
+      // $gebarentolkButton.href = urls.gebarentolk
 
       break
     }
